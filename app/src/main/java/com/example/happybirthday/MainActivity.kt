7c6fd9610886b4086c5ba1b981b4a2d26ddfb136
rodionovmax <rodionovmax90test@gmail.com>
2023-07-06 21:46:56 -0400

    BusinessCard

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.example.happybirthday
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -25,6 +32,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+const val greenFontInt = 0xFF1D5A1D
+const val greenBackgroundInt = 0xFFD2E8D4
+const val blueAvatarBackgroundInt = 0xFF063042
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,15 +47,121 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(
-                        message = stringResource(R.string.happy_birthday_text),
-                        from = stringResource(
-                            R.string.signature_text
-                        )
-                    )
+                    BusinessCard()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun BusinessCard(modifier: Modifier = Modifier) {
+    Box(
+        modifier
+            .fillMaxSize()
+            .background(Color(greenBackgroundInt))
+    ) {
+        PersonalInformation(
+            fullName = stringResource(R.string.full_name_text),
+            title = stringResource(R.string.title_text),
+            modifier = modifier.align(Alignment.Center)
+        )
+        ContactInformation(
+            modifier = modifier
+                .padding(bottom = 50.dp)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun Avatar(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .width(120.dp)
+            .aspectRatio(1f)
+            .background(color = Color(blueAvatarBackgroundInt), shape = RectangleShape),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.android_logo),
+            contentDescription = "Android logo",
+            modifier
+                .width(100.dp)
+                .aspectRatio(1f)
+        )
+    }
+}
+
+@Composable
+fun PersonalInformation(fullName: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Avatar()
+        Text(
+            text = fullName,
+            fontSize = 36.sp
+        )
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            color = Color(greenFontInt)
+        )
+    }
+}
+
+@Composable
+fun ContactInformation(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column {
+            ContactType(
+                icon = R.drawable.ic_phone_24,
+                iconContentDescription = stringResource(R.string.phone_icon_content_desc),
+                contactInfo = stringResource(R.string.contact_phone_text)
+            )
+            ContactType(
+                icon = R.drawable.ic_share_24,
+                iconContentDescription = stringResource(R.string.share_icon_content_desc),
+                contactInfo = stringResource(R.string.contact_share_text)
+            )
+            ContactType(
+                icon = R.drawable.ic_email_24,
+                iconContentDescription = stringResource(R.string.email_icon_content_desc),
+                contactInfo = stringResource(R.string.contact_email_text)
+            )
+        }
+    }
+}
+
+@Composable
+fun ContactType(icon: Int, iconContentDescription: String, contactInfo: String) {
+    Row(
+        Modifier
+            .padding(vertical = 6.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = iconContentDescription,
+            Modifier
+                .width(24.dp)
+                .aspectRatio(1f),
+            tint = Color(greenFontInt) // how to extract Color to outside var
+        )
+        Text(
+            text = contactInfo,
+            Modifier
+                .padding(horizontal = 24.dp)
+        )
     }
 }
 
@@ -97,11 +214,6 @@ fun GreetingText(
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingImage(
-            message = stringResource(R.string.happy_birthday_text),
-            from = stringResource(
-                R.string.signature_text
-            )
-        )
+        BusinessCard()
     }
 }
